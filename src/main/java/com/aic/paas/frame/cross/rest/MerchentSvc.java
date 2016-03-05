46adeba2dde13d18339222d996ee7cbb27c07b5c
oriiro <mkoh2c03@163.com>
2016-03-05 16:15:35 +0800

    修改用户分配角色界面分页查询

--- src/main/java/com/aic/paas/frame/cross/rest/MerchentSvc.java
+++ src/main/java/com/aic/paas/frame/cross/rest/MerchentSvc.java
@@ -121,11 +121,14 @@ public interface MerchentSvc {
 	 * 分页查询
 	 * @param pageNum : 指定页码
 	 * @param pageSize : 指定页行数
+	 * @param orgId 指定组织
+	 * @param direct 是否直属组织, 为null表示不区分
+	 * @param admin 是否管理员, 为null表示不区分
 	 * @param cdt : 条件对象
 	 * @param orders : 排序字段, 多字段以逗号分隔
 	 * @return 操作员表[SYS_OP]分页列表对象
 	 */
-	public Page<SysOpInfo> queryOpInfoPage(Integer pageNum, Integer pageSize, CSysOp cdt, String orders);
+	public Page<SysOpInfo> queryOpInfoPage(Integer pageNum, Integer pageSize, Long orgId,Boolean direct,Boolean admin ,CSysOp cdt, String orders);
 
 
 	
