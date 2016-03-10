e423da7b2f7fc1687a83b00801d72f956c7c116b
wanwb <wanwb@asiainfo.com>
2016-03-10 10:15:49 +0800

    修改单点登录BUG

--- src/main/java/com/aic/paas/frame/cross/mvc/IndexMvc.java
+++ src/main/java/com/aic/paas/frame/cross/mvc/IndexMvc.java
@@ -1,7 +1,13 @@
 package com.aic.paas.frame.cross.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.binary.framework.util.ControllerUtils;
 
 
 @Controller
@@ -11,6 +17,22 @@ public class IndexMvc {
 	@RequestMapping("/")
 	public String index() {
 		return "redirect:/index.jsp";
+	}
+	
+	
+	
+	
+	
+	@RequestMapping("/invalid")
+	public void invalid(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			HttpSession session = request.getSession(false);
+			if(session != null) {
+				session.invalidate();
+			}
+		}catch(Exception e) {
+		}
+		ControllerUtils.returnJson(request, response, true);
 	}
 	
 	
